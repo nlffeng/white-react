f7c2e1f8f3b7e1fdd17a4cfa724e30e763d7b316
whitefon <nlffeng@163.com>
2019-10-14 23:41:28 +0800

    feat:增加 git hooks 提交前 lint

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -3,15 +3,12 @@
  */
 
 import React from 'react';
-import Test from '../../components/Test';
-
 import './index.scss';
 
 function App() {
   return (
     <div className="app-container">
       App
-      <Test />
     </div>
   );
 }
